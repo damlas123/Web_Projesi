ce8e13e33000492c786c476da8d0144ccad0d651
Damla Söylemez <damlasoylemez321@gmail.com>
2025-04-30 00:26:24 +0300

    update

--- Web_proje/login.php
+++ Web_proje/login.php
@@ -1,14 +1,25 @@
 <?php
-
-$kullanici = "b241210351@ogr.sakarya.edu.tr";
+$kullaniciAdi = "b241210351@ogr.sakarya.edu.tr";
 $sifre = "123456";
 
-if ($_POST['name'] === $kullanici && $_POST['password'] === $sifre) {
-    header("Location: hakkımda.html");
-    exit;
-} else {
-    echo "Hatalı kullanıcı adı veya şifre.";
-    exit;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $gelenAd = trim($_POST['email']);
+    $gelenSifre =trim( $_POST['password']);
+
+    if ($gelenAd === $kullaniciAdi && $gelenSifre === $sifre) {
+        $sadeAd = explode('@', $gelenAd)[0];
+        echo "<h2>Hoşgeldiniz $sadeAd</h2>";
+        echo "<script>
+                setTimeout(function() {
+                    window.location.href = 'hakkımda.html';
+                }, 2000);
+              </script>";
+    } else {
+        echo "<script>
+                alert('Hatalı kullanıcı adı veya şifre!');
+                window.location.href = 'login.html';
+              </script>";
+        exit;
+    }
 }
 ?>
-
